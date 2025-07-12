32723cfcfc0664a88b7afa68eb101687862995e9
Baishali Chatterjee <baishalichatterjee991@gmail.com>
2025-07-12 23:54:11 +0530

    day 12 first commit

---
+++ Day12/integer_to_roman.py
@@ -0,0 +1,16 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        ans = '';
+        s = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+        n = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+        i=0;
+        while num > 0:
+            if num >= n[i]:
+                ans += s[i];
+                num -= n[i];
+            else:
+                i+=1;
+        return ans;
+
+
+        
