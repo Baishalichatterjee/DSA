371b7a25181b538bfe77fbe5746620c19470a050
Baishali Chatterjee <baishalichatterjee991@gmail.com>
2025-07-15 18:35:24 +0530

    day 14 second commit

---
+++ Day14/add_binary.py
@@ -0,0 +1,19 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        alen=len(a);
+        blen=len(b);
+        carry=0;
+        ans="";
+        i=0;
+        while i<alen or i<blen or carry==1:
+            x=0;
+            if i<alen and a[alen-i-1]=='1':
+                x=1;
+            y=0;
+            if i<blen and b[blen-i-1]=='1':
+                y=1;
+            ans = str((x+y+carry)%2) + ans;
+            carry = ((x+y+carry)//2);
+            i+=1;
+        return ans;
+                    
